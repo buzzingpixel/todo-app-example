30c30c4fada0c6505ed0f21874cc65a428ff74bd
TJ Draper <tj@buzzingpixel.com>
2023-08-27 07:36:27 -0500

    Set up tailwind

---
+++ src/App.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+class App
+{
+    public const PATH = __DIR__;
+}
--- src/GetHelloWorldAction.php
+++ src/GetHelloWorldAction.php
@@ -4,16 +4,26 @@ declare(strict_types=1);
 
 namespace App;
 
+use BuzzingPixel\Templating\TemplateEngineFactory;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
 readonly class GetHelloWorldAction
 {
+    public function __construct(
+        private TemplateEngineFactory $templateEngineFactory,
+    ) {
+    }
+
     public function __invoke(
         ServerRequestInterface $request,
         ResponseInterface $response,
     ): ResponseInterface {
-        $response->getBody()->write('Hello world!');
+        $response->getBody()->write(
+            $this->templateEngineFactory->create()
+                ->templatePath(__DIR__ . '/HelloWorld.phtml')
+                ->render(),
+        );
 
         return $response;
     }
